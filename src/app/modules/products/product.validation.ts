d15bdfd5c96beae6a9823c67a80dad5b5ed5a612
programarreza <programarreza@gmail.com>
2024-07-10 00:28:56 +0600

    product created successfully

--- src/app/modules/products/product.validation.ts
+++ src/app/modules/products/product.validation.ts
@@ -3,17 +3,14 @@ import { Category } from "./product.constant";
 
 const createProductValidationSchema = z.object({
   body: z.object({
-    password: z.string().max(20),
-    admin: z.object({
-      name: z.string(),
-      category: z.enum([...Category] as [string, ...string[]]),
-      stockQuantity: z.number(),
-      brand: z.string(),
-      rating: z.number(),
-      description: z.string(),
-      price: z.number(),
-      image: z.string(),
-    }),
+    name: z.string(),
+    category: z.enum([...Category] as [string, ...string[]]),
+    stockQuantity: z.number(),
+    brand: z.string(),
+    rating: z.number(),
+    description: z.string(),
+    price: z.number(),
+    image: z.string(),
   }),
 });
 
